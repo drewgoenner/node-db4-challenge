dee434511437ca6483739f65ecb9bbfb349b72bf
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2019-10-10 15:09:22 -0500

    built model in browser, did migrations to create tables

---
+++ data/migrations/20191010145234_bootstrap.js
@@ -0,0 +1,80 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('recipes', tbl => {
+        
+        tbl
+        .increments();
+        
+        tbl
+        .string('dish', 255)
+        .notNullable();
+    })
+
+    .createTable('instructions', tbl => {
+        
+        tbl
+        .increments();
+
+        tbl
+        .text('instructions')
+        .notNullable();
+
+        tbl
+        .integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('recipes')
+        .onUpdate('CASCADE')
+        .onDelete('RESTRICT');
+    })
+
+    .createTable('ingredients', tbl => {
+        
+        tbl
+        .increments();
+        
+        tbl
+        .string('ingredient', 255)
+        .notNullable();
+    })
+    
+    .createTable('ingredient_quantities', tbl => {
+        
+        tbl
+        .increments();
+        
+        tbl
+        .string('quantity', 255)
+        .notNullable();
+        
+        tbl
+        .integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('recipes')
+        .onUpdate('CASCADE')
+        .onDelete('RESTRICT')
+        
+        tbl
+        .integer('ingredient_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('ingredients')
+        .onUpdate('CASCADE')
+        .onDelete('RESTRICT');
+    })
+  
+};
+
+exports.down = function(knex) {
+
+    return knext.schema
+    .dropTableIfExists('ingredient_quantities')
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('instructions')
+    .dropTableIfExists('recipes')
+  
+};
